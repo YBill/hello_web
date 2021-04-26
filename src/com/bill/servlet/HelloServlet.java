af90dd3594b4632221be6f8c5f47d831cafd1196
Bill <yuanweibiao@pdnews.com>
2021-04-26 17:07:17 +0800

    支持 Servlet

---
+++ src/com/bill/servlet/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.bill.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        message = "Hello, Servlet!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>" + message + "</h1>");
+
+    }
+}
